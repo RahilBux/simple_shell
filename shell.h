935b2b565345ef54a9ffa88d4e2f4d459ed26b93
RahilBux <rahilbux@gmail.com>
2023-08-12 15:35:50 +0200

    added shell.h

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#define BUFFER_SIZE 256
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+
+#endif
