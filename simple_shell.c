ebceb58728785676d29b7f4f7735643e25eb72e7
RahilBux <rahilbux@gmail.com>
2023-08-13 04:17:53 -0700

    dawda

--- simple_shell.c
+++ simple_shell.c
@@ -14,7 +14,7 @@ int simple_shell(void)
 		read = getline(&buf, &inp_len, stdin);
 		if (read == -1)
 		{
-			printf("Error reading line\n");
+			perror("./shell");
 			free(buf);
 			return (1);
 		}
@@ -22,22 +22,14 @@ int simple_shell(void)
 		if (buf[read - 1] == '\n')
 			buf[read - 1] = '\0';
 
-		if (strcmp(buf, "exit") == 0)
-		{
-			printf("Exiting Shell...\n");
-			free(buf);
-			return (0);
-		}
-
 		pid = fork();
 
 		if (pid < 0)
-			printf("Error with fork\n");
+			perror("./shell");
 		
 		if (pid != 0)
 		{
 			wait(&status);
-			continue;
 		}
 		else
 		{
