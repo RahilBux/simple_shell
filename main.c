82b2c7f4ba63d5103818d39719c54a154ffd6e4d
RahilBux <rahilbux@gmail.com>
2023-08-23 01:13:25 +0200

    new

--- main.c
+++ main.c
@@ -1,18 +1,79 @@
 #include "shell.h"
 
 /**
+ * execute_command - executes a command in noninteractive shell
+ * @args: command to execute
+ * @env: environment to pass
+ */
+
+void execute_command(char **args, char **env)
+{
+	pid_t pid;
+	int status;
+
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("fork");
+	}
+	else if (pid == 0)
+	{
+		if (execve(args[0], args, env) == -1)
+		{
+			perror("Execution failed");
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+		wait(&status);
+}
+
+/**
  * main - Creates a simple shell environment
- * @ac: Argument count
- * @av: Array of pointers to string of arguments
+ * @argc: Argument count
+ * @argv: Array of pointers to string of arguments
  * @env: Array of strings to the environment
  *
  * Return: 0(Success)
  */
 
-int main(int ac, char **av, char **env)
+int main(int argc, char *argv[], char **env)
 {
-	(void) ac;
-	(void) av;
-	simple_shell(env);
+	char *line = NULL;
+	size_t line_size = 0;
+	ssize_t read_size;
+	char *args[10];
+	int arg_count;
+	char *token;
+	(void) argc;
+	(void) argv;
+
+	if (isatty(STDIN_FILENO))
+	{
+		simple_shell(env);
+	}
+	else
+	{
+		while ((read_size = getline(&line, &line_size, stdin)) != -1)
+		{
+			line[strcspn(line, "\n")] = '\0';
+
+			token = strtok(line, " ");
+			arg_count = 0;
+			while (token != NULL && arg_count < 10 - 1)
+			{
+				args[arg_count] = token;
+				arg_count++;
+				token = strtok(NULL, " ");
+			}
+			args[arg_count] = NULL;
+
+			if (arg_count > 0)
+			{
+				execute_command(args, env);
+			}
+		}
+		free(line);
+	}
 	return (0);
 }
--- shell.h
+++ shell.h
@@ -7,6 +7,10 @@
 #include <sys/wait.h>
 #include <string.h>
 #include <sys/types.h>
+#include <errno.h>
+#include <fcntl.h>
+
+#define MAX_INPUT 100
 
 void simple_shell(char **env);
 char **tok(char *ptr);
@@ -15,5 +19,7 @@ int _strcmp(char *str1, char *str2);
 void printbash(void);
 int _strlen(char *str);
 char *pathcheck(char *str);
+void nonshell(char **env, char **av);
+void execute_command(char **args, char **env);
 
 #endif
